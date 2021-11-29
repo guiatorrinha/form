9a1460a4352194c3520e33a14c6bc12372cfb658
henriq4 <henriq4NET@gmail.com>
2021-11-29 20:43:04 -0300

    Implement number masc

--- scripts/mascaras/number.js
+++ scripts/mascaras/number.js
@@ -1,3 +1,20 @@
 const data = require("../constants").default
 
-const {dddTelefone, numberoTelefone} = data
+const {numberoTelefone: number} = data
+
+const celular = number.lenght === 9
+
+  if (celular) {
+    const number_slice_1 = number.slice(0,5)
+    const number_slice_2 = number.slice(5,9)
+    const number_final = `${number_slice_1}-${number_slice_2}`
+  }
+
+  else {
+    const number_slice_1 = number.slice(0,4)
+    const number_slice_2 = number.slice(4,8)
+    const number_final = `${number_slice_1}-${number_slice_2}`
+  }
+
+const numberoTelefone = number_final
+
