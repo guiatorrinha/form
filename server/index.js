211032ecc7c2cb22450c5d9c1d76204e170daea8
henriq4 <henriq4NET@gmail.com>
2021-12-01 16:13:37 -0300

    chore: implementig body-parser

--- server/index.js
+++ server/index.js
@@ -1,18 +1,26 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const { urlencoded } = require('body-parser')
 
 const app = express()
 const port = 4444
 
+app.use(urlencoded({extended: false}))
+app.use(bodyParser.json())
 app.use(express.static(__dirname + "../"))
 app.use(express.static('public'))
 
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => {
-  res.render('index')
+app.get('/form', (req, res) => {
+  return res.render('form')
+})
+
+app.post('/business', (req, res) => {
+  console.log(req.body)
+  res.render('business')
 })
 
 app.listen(port, () => {
-  console.log("server listen..")
+  console.log("server listen in http://localhost:4444..")
 })
